fb94534e52c5fa2bdc8054de3801eefc917883a4
viscountfam <V3ctor42!>
2019-11-13 18:23:42 -0500

    I finised the main part of the assignment, I may finish the flex goals later

--- index.js
+++ index.js
@@ -39,8 +39,24 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+}
+Person.prototype.eat = function(someFood) {
+  if(this.stomach.length <= 9){
+  return this.stomach.push(someFood);
+  } else{
+    return this.stomach;
+  }
+}
+Person.prototype.poop = function() {
+   this.stomach = [];
+}
 
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`
 }
 
 /*
@@ -57,9 +73,23 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.tank = 0,
+  this.odometer = 0,
+  this.model = model,
+  this.milesPerGallon = milesPerGallon
 }
+Car.prototype.fill = function(gallons) {
+  return this.tank += gallons;
+}
+Car.prototype.drive = function(dist){
+  this.odometer += dist;
+  this.tank -= dist;
+  if(this.tank - dist <=0){
+    return `I ran out of fuel at ${this.odometer} miles!`
+  }
+  return this.odometer;
+ }
 
 /*
   TASK 3
@@ -68,8 +98,17 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age, stomach, favoriteToy);
+  this.favoriteToy = favoriteToy;
+}
+ Baby.prototype = Object.create(Person.prototype);
+  /*Object.defineProperty(Baby.prototype, 'constructor', {
+    value: Baby,
+    enumerable: false,
+    writable: true });*/
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
 }
 
 /* 
